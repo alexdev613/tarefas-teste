47c4d78535b46559c4877b382a89d00e97d890bd
Alex Nascimento <alexjose68@hotmail.com>
2025-02-04 19:11:04 -0300

    feat: Me situando nas funcionalidades do React Native, seus componentes e estilo com StyleSheet, usando também o tão conhecido hook useState do React.

--- App.js
+++ App.js
@@ -1,22 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [nome, setNome] = useState("Fulano");
+
+  function handleMudaNome() {
+    setNome("Alex Heisenberg");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>
-        Sujeito Programador!
-      </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Alex</Text>
+      <Text style={[styles.title, styles.text]}>Camarada Programador: {nome}</Text>
+      
+      <TouchableOpacity style={styles.button} onPress={handleMudaNome}>
+        <Text style={styles.buttonText}>Mudar Nome</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#343434',
+    paddingTop: 28,
   },
-});
+  title: {
+    fontSize: 32,
+    color: "#f1f1f1",
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  text: {
+    color: "red"
+  },
+  button: {
+    backgroundColor: 'blue',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold'
+  }
+})
